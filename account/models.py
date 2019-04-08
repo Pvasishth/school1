f3d5925ebb4e06d5740b3d19318cae5910cd2145
Paras <paras.vasishth@gmail.com>
2019-04-08 14:44:38 +0530

    username and password model added

--- account/models.py
+++ account/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django import forms
 from django.core.validators import RegexValidator
 
 class SchoolProfile(models.Model):
 
   principal = models.OneToOneField(User,on_delete = models.CASCADE, related_name = 'schoolprofile')
   school_name = models.CharField(max_length = 20 , null =True ,blank = True)
-
+  username = models.CharField(max_length=25, unique=True, default='')
+  password = models.CharField(max_length=30, default='')
   #address for the school
   state = models.CharField(max_length = 20, null =True ,blank = True)
   city = models.CharField(max_length = 20 , null =True ,blank = True)
