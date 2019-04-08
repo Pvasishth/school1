c927153a5825af3330af3074c186d944d278b102
Paras <paras.vasishth@gmail.com>
2019-04-08 14:22:01 +0530

    database setting and req.txt

--- vidyarthi/settings.py
+++ vidyarthi/settings.py
@@ -79,12 +79,27 @@ WSGI_APPLICATION = 'vidyarthi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'root1234',
+            'HOST': '35.200.196.46',
+            'PORT': '5432'
+        }
 }
+
+
+
 
 
 # Password validation
